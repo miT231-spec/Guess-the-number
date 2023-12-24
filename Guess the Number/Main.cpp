ec8e851b1228d3a57d1a1bd0cce3487a9a4a4d97
miT231-spec <85369375+miT231-spec@users.noreply.github.com>
2023-12-24 22:45:36 +0100

    fixed spelling

--- Guess the Number/Main.cpp
+++ Guess the Number/Main.cpp
@@ -8,13 +8,13 @@ int main()
 
 	time_t current_time = time(NULL);  // creating a timestamp
 
-	srand((unsigned)time(NULL)); // passing timestamp to srand to generate random number
+	srand((unsigned)time(NULL)); 	// passing timestamp to srand to generate random number
 
+	int randomAnsw = rand() % 11;	// creating a random number between 0-10
+	
 	int attempts;		// initializing attempts
-	cout << "how many attempts do you want to have?\n";			// printing text
+	cout << "how many attempts do you want to have?\n";	// printing text
 	cin >> attempts;	// saving the number of attempts
-
-	int randomAnsw = rand() % 11;		// creating a random number between 0-10
 
 	for (int i = 0; i < attempts; i++)	 // as long as "i" is smaller than the inserted attempts the loop runs (initializing the loop)
 	{
@@ -24,7 +24,7 @@ int main()
 		if (userInput == randomAnsw)	// if userInput is the answer we do the following
 		{
 			cout << "you won!\n";		// printing stuff
-			break;						// breaking the loop if the user guessed right
+			break;				// breaking the loop if the user guessed right
 		}
 		else	// if the userInput was false we print "try again" 
 		{
